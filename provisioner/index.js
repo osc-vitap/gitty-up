7e1684147cfabf10910c7f9d841fd42577f05e38
Tanvish <101194211+TanvishGG@users.noreply.github.com>
2024-10-24 00:25:54 +0530

    split the code into multiple functions and added function to make nginx conf files

--- provisioner/index.js
+++ provisioner/index.js
@@ -2,12 +2,21 @@ import dotenv from 'dotenv';
 import fs from 'fs';
 dotenv.config();
 
-let records = [];
 
+async function main() {
 const participants = fs.readFileSync("./data.csv", "utf8").split("\n").filter(x => x !== "").map((line) => line.split(", ").map((item) => item.trim()));
-
+let records = [];
 for (const participant of participants) {
     if (participant[0] === "registration_number") continue;
+    console.log('Provisioning', participant[0]);
+    appendRecord(participant);
+    createConfFile(participant);
+}
+await postRecords(records);
+}
+
+
+function appendRecord(participant) {
     records.push({
         "name": `${participant[0]}.oscvitap.org`,
         "proxied": true,
@@ -19,15 +28,37 @@ for (const participant of participants) {
         "type": "A"
     })
 }
-console.log(records);
-let data = await fetch(`https://api.cloudflare.com/client/v4/zones/${process.env.CLOUDFLARE_ZONE_ID}/dns_records/batch`, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'X-Auth-Email': process.env.CLOUDFLARE_EMAIL,
-        'X-Auth-Key': process.env.CLOUDFLARE_API_KEY
-    },
-    body: JSON.stringify({ posts: records })
-});
-console.log(await data.json());
 
+
+async function postRecords(records) {
+    let data = await fetch(`https://api.cloudflare.com/client/v4/zones/${process.env.CLOUDFLARE_ZONE_ID}/dns_records/batch`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Auth-Email': "process.env.CLOUDFLARE_EMAIL",
+            'X-Auth-Key': "process.env.CLOUDFLARE_API_KEY"
+        },
+        body: JSON.stringify({ posts: records })
+    });
+    if (data.status === 200) {
+        console.log("Records added successfully");
+    } else {
+        console.log("Error adding records", await data.json());
+    }
+}
+
+
+function createConfFile(participant) {
+    fs.writeFileSync(`${process.env.NGINX_INCLUDE_DIR}/${participant[0]}.conf`,
+        `server {
+            listen 443 ssl;
+            server_name ${participant[0]}.oscvitap.org;
+        
+            location / {
+                proxy_pass 127.0.0.1:${participant[2]};
+            }
+        }`
+    )
+}
+
+await main();
